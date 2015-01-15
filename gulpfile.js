016ea39897677b1e9d7682a8f8ffcdb11cf300fa
Ross Noble <rosshnoble@gmail.com>
2015-01-15 18:52:36 -0500

    Minify js

--- gulpfile.js
+++ gulpfile.js
@@ -4,19 +4,9 @@ var concat = require('gulp-concat');
 
 gulp.task('default', function () {
   gulp.src([
-      'bower_components/jquery/dist/jquery.js',
-      'bower_components/jquery-ui/jquery-ui.js',
-      'lib/*.js'
-    ])
-    .pipe(concat('index.js'))
-    .pipe(gulp.dest('./app/js'));
-});
-
-gulp.task('production', function () {
-  gulp.src([
-      'bower_components/jquery/dist/jquery.js',
-      'bower_components/jquery-ui/jquery-ui.js',
-      'lib/*.js'
+      './bower_components/jquery/dist/jquery.js',
+      './bower_components/jquery-ui/jquery-ui.js',
+      './lib/*.js'
     ])
     .pipe(uglify())
     .pipe(concat('index.js'))
