54730091dcd35207fc8dd6e5580ecf19c8e212ce
Ross Noble <rosshnoble@gmail.com>
2017-08-16 18:02:43 -0700

    Add Line component and faux markdown styling

---
+++ app/components/Terminal/Line/Line.js
@@ -0,0 +1,48 @@
+import React, { PureComponent } from 'react'
+import css from './Line.scss'
+
+class Line extends PureComponent {
+
+  render () {
+    const {
+      children
+    } = this.props
+
+    let body = children
+    let tag = null
+
+    // TODO: Refactor
+    if (children) {
+      const parts = children.split(/([\*\#])+\s?/)
+
+      if (parts.length > 1) {
+        const prefix = parts[1]
+        body = parts[2]
+
+        if (prefix) {
+          if (prefix === "*") {
+            tag = <span className={css.Bullet}>{prefix}</span>
+          } else if (prefix.match(/#/)) {
+            tag = <span className={css.Header}>{prefix}</span>
+          }
+        }
+      }
+    }
+
+    return (
+      <div className={css.Line}>
+        <div className={css.LineNumber}>
+          {this.props.number}
+        </div>
+
+        <div className={css.Content}>
+          {tag && tag}
+          {' '}
+          {body}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Line
--- app/components/Terminal/Vim/Vim.js
+++ app/components/Terminal/Vim/Vim.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import data from 'app/data.json'
+import Line from '../Line/Line'
 import css from './Vim.scss'
 
 class Vim extends Component {
@@ -7,6 +8,13 @@ class Vim extends Component {
   render () {
     return (
       <div className={css.Vim}>
+        <Line number={1}># Ross Haliburton Noble</Line>
+        <Line number={2}></Line>
+        <Line number={3}>## About / Work</Line>
+        <Line number={4}></Line>
+        <Line number={5}>* Software developer from Vancouver, Canada</Line>
+        <Line number={6}>* Formerly of PasswordBox, Intel, Rocksauce</Line>
+
         <div className={css.StatusBar}>
           <div className={css.StatusTop}>
             {data.filename}
